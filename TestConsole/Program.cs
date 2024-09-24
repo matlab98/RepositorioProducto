68e5352701d464319eab56978c9e652a888f8213
Hilder Brando Arrieta Ruiz <hilder.arrieta@gse.com.co>
2024-09-24 11:06:45 -0500

    Initial commit

---
+++ TestConsole/Program.cs
@@ -0,0 +1,62 @@
+﻿using TestConsole.Entities;
+using TestConsole.Service;
+
+
+var productoService = new ProductoService();
+
+while (true) {
+    var opcion = Console.ReadLine();
+    switch (opcion)
+{
+    case "1":
+        try
+        {
+            Console.WriteLine("Nombre del producto: ");
+            var nombre = Console.ReadLine();
+            Console.WriteLine("Precio del producto: ");
+
+
+            if (!int.TryParse(Console.ReadLine(), out var precio))
+            {
+                throw new Exception("El precio debe ser un número");
+            }
+
+            
+            var producto = new Producto { Nombre = nombre, Precio = precio };
+            var agregarProducto = productoService.AgregarProducto(producto);
+            Console.WriteLine(agregarProducto.ToString());
+
+        }catch(Exception ex)
+        {
+            Console.WriteLine($"Error {ex.Message}");
+        }
+        break;
+
+    case "2":
+        try
+        {
+            Console.WriteLine("El producto más caro: ");
+            var obtenerMasCaro = productoService.ObtenerProductoMasCaro();
+            Console.WriteLine(obtenerMasCaro.Nombre);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error {ex.Message}");
+        }
+        break;
+    case "3":
+        try
+        {
+            Console.WriteLine("Precio promedio es: ");
+            var calcularPromedio = productoService.calcularPrecioPromedio();
+            Console.WriteLine(calcularPromedio);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error {ex.Message}");
+        }
+        break;
+    }
+}
---
+++ TestConsole/Service/ProductoService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestConsole.Entities;
+
+namespace TestConsole.Service
+{
+
+    public class ProductoService
+    {
+        private readonly List<Producto> _producto = new List<Producto>();
+
+
+        public string AgregarProducto(Producto producto)
+        {
+            _producto.Add(producto);
+
+            return "Producto agregado";
+        }
+
+        public Producto ObtenerProductoMasCaro()
+        {
+            try
+            {
+                if (!_producto.Any())
+                {
+                    throw new InvalidOperationException();
+                }
+                return _producto.OrderByDescending(p => p.Precio).FirstOrDefault();
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new Exception($"Error {ex.Message}");
+            }
+        }
+
+        public decimal calcularPrecioPromedio()
+        {
+            try
+            {
+                if (!_producto.Any())
+                {
+                    throw new InvalidOperationException();
+                }
+                return _producto.Average(p => p.Precio);
+            }
+            catch (InvalidOperationException ex)
+            {
+                throw new Exception($"Error {ex.Message}");
+            }
+        }
+    }
+}
